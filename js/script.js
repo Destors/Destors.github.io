48d7a18b31114c83aee38ffd5060dfb7a84553d1
Alex <halen54001@gmail.com>
2018-07-07 12:31:29 +0300

    medium

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
-var moreButtonEvent = function() {
-    if(this.getAttribute('data-more') == 0) {
+var moreButtonEvent = function () {
+  if (this.getAttribute('data-more') == 0) {
     this.setAttribute('data-more', 1);
     this.innerHTML = 'Читать меньше';
     this.previousSibling.previousSibling.style.display = 'block';
-  }
-
-  else if(this.getAttribute('data-more') == 1) {
+  } else if (this.getAttribute('data-more') == 1) {
     this.setAttribute('data-more', 0);
     this.innerHTML = 'Читать больше';
     this.previousSibling.previousSibling.style.display = 'none';
-  } 
+  }
 };
 
 var moreButtonsList = document.getElementsByClassName('page-main__button');
 
 for (var i = 0; i < moreButtonsList.length; i++) {
-    moreButtonsList[i].addEventListener('click', moreButtonEvent);
+  moreButtonsList[i].addEventListener('click', moreButtonEvent);
 }

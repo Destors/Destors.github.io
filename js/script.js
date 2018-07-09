38c2795d6c12230b88700b47a5c3a436f116b9bb
Alex <halen54001@gmail.com>
2018-07-09 19:56:08 +0300

    medium_1.6

--- js/script.js
+++ js/script.js
@@ -15,3 +15,32 @@ var moreButtonsList = document.getElementsByClassName('page-main__button');
 for (var i = 0; i < moreButtonsList.length; i++) {
   moreButtonsList[i].addEventListener('click', moreButtonEvent);
 }
+
+ var link = document.querySelector(".social-list__button");
+
+  var popup = document.querySelector(".modal");
+
+  var close = popup.querySelector(".modal-close-btn");
+
+  var login = popup.querySelector("[name=username]");
+
+
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    username.focus();
+  });
+
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+  });
+
+ window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+      }
+    }
+  });
